67e249909dd68354f92c307019d04e5253e4bf61
shajib126 <shajib126@gmail.comm>
2021-08-17 12:21:12 +0600

    project don

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function compute() {
 	var principal = document.getElementById("principal").value;
+
 	var rate = parseFloat(document.getElementById("rate").value);
 
 	var years = document.getElementById("years").value;
 	var interest = (principal * years * rate) / 100;
 	var year = new Date().getFullYear() + parseInt(years);
-	document.getElementById("result").innerText = `if you deposit ${principal} 
+	if (principal > 0) {
+		document.getElementById("result").innerText = `if you deposit ${principal}
      at an interest rate of ${rate}%
      You will receive an amoun of ${interest}
      in the year of ${year}
      `;
+	} else {
+		alert("enter a positive number");
+	}
 }
 
 function updateRate() {
